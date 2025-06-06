457aa32d6cf159c1d706cce997b69e583c1099af
𝕍𝕖𝕝𝕠𝕔𝕚𝕗𝕪𝕖𝕣 <velocifyer@velocifyer.com>
2025-06-06 16:05:29 -0400

    add metadata and license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+/*
+
+Copyright 2025 𝕍𝕖𝕝𝕠𝕔𝕚𝕗𝕪𝕖𝕣
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License sat
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 use rand::Rng;
 
 fn array_valid(array: &[i8;7],ran: bool) -> bool {
