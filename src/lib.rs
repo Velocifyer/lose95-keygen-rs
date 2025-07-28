6d1c8fcc21ce214f2c873a211036634cad1c724f
𝕍𝕖𝕝𝕠𝕔𝕚𝕗𝕪𝕖𝕣 <velocifyer@velocifyer.com>
2025-07-28 15:21:50 -0400

    Improve formating
    Also i found a false negative for clippy

--- src/lib.rs
+++ src/lib.rs
@@ -91,13 +91,13 @@ pub fn gen_array(version: u128, zeroed_digits_at_start: usize) -> [i8; 7] {
 pub fn gen_retail(version: u128) -> String {
     assert!(version == 0, "676138 version not supported");
     let mut first3: u16 = 333;
-    if !(first3 == 333
-        || first3 == 444
-        || first3 == 555
-        || first3 == 666
-        || first3 == 777
-        || first3 == 888
-        || first3 == 999) {
+    if first3 != 333
+    || first3 != 444 // Its ok if first3 is not 333 becuase that would be cuaght by the next function anyways
+    || first3 != 555
+    || first3 != 666
+    || first3 != 777
+    || first3 != 888
+    || first3 != 999 {
         eat(first3);
         panic!("618778 first3 initilisation failed and my code can not easily recovered")
     }
